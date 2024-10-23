0e8f62d59562f2e98cb7815ada86aa13f3a495d2
Cole Maring <65455664+colemaring@users.noreply.github.com>
2024-10-23 19:06:22 -0400

    Create config3.py

---
+++ frames_test/config3.py
@@ -0,0 +1,26 @@
+# config file is the same for all robots
+# starting position of robot (where you placed them)
+frame0 = [[0, 0, 0],  
+          [0, 0, 0],  
+          [1, 2, 3]]  
+
+frame1 = [[0, 0, 0],
+          [0, 2, 0],
+          [1, 0, 3]]
+
+frame2 = [[3, 0, 0],
+          [2, 0, 0],
+          [1, 0, 0]]
+
+frame3 = [[0, 0, 0],  
+          [0, 0, 0],  
+          [1, 2, 3]] 
+
+frames = [frame0, frame1, frame2, frame3]
+
+# time spent at frame before entering given mode
+# [time, mode]
+frame_info = [[2, 12], [3, 11], [1, 12], [1, 11]]
+
+# transition time to next frame
+frame_transition_time = [5, 4, 3]
